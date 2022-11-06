132d0f9779d085b6d9672cf2968919fa98f77d92
Ridjejr <esdrasmissilou@gmail.com>
2022-11-06 15:14:58 +0100

    le queryBuilder

--- src/Controller/AlbumController.php
+++ src/Controller/AlbumController.php
@@ -13,7 +13,7 @@ class AlbumController extends AbstractController
      */
     public function listeAlbums(AlbumRepository $repo)
     {
-        $albums=$repo->findBy(['date'=>'2006'],['nom'=>'asc'],5);
+        $albums=$repo->listeAlbumsComplete();
         return $this->render('album/listeAlbums.html.twig', [
             'lesAlbums' => $albums,
         ]);
